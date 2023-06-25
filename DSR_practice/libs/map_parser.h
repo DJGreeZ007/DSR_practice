36c085d15bf4c67b7b59a39d04274aa5d8a99cf3
DJ_GreeZ <makarov.nikita.o@yandex.ru>
2023-06-25 13:59:12 +0300

    Add project files.

---
+++ DSR_practice/libs/map_parser.h
@@ -0,0 +1,10 @@
+#ifndef MAP_PARSER_H
+#define MAP_PARSER_H
+
+namespace mp {
+	class Map_parser {
+
+	};
+}
+
+#endif //MAP_PARSER_H
---
+++ DSR_practice/libs/node_and_link.cpp
@@ -0,0 +1,14 @@
+#include "node_and_link.h"
+
+using namespace nal;
+
+
+/* Node */
+
+Node::Node(std::string _id) :id{ _id } {};
+
+Node::Node(std::string _id, std::uint16_t _addr, Nwk_type _device_type, std::string _label) :
+	completed{ true }, id{ _id }, addr{ _addr }, device_type{ _device_type }, label{ _label } {}
+
+/* Link */
+
