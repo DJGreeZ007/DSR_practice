098a134ddbbf81ec02b8ea975b988473939bfbe8
DJGreeZ007 <127106317+DJGreeZ007@users.noreply.github.com>
2023-06-26 13:49:57 +0300

    Comments changed

--- DSR_practice/libs/map_parser.h
+++ DSR_practice/libs/map_parser.h
@@ -2,30 +2,30 @@
 #define MAP_PARSER_H
 
 
-//libs
+/* Libs */
 #include "node_and_link.h"
 
-//c++ libs
+/* C++ libs */
 #include <vector>
 #include <fstream>
 
-using namespace nal;						//Node and Link classes
+using namespace nal;                                               /* Node and Link classes */
 
 namespace mp {
 
-	//map_parser error
+	/* Map_parser error */
 	enum map_parser_error {
 		NO_ERRORS,
-		ERROR_FILE_OPENING,							//Error opening the file
-		ERROR_READING_FILE_HEADER,					//Error reading the file header
-		ERROR_INCORRECT_DATA_IN_FILE,				//Incorrect data in the file
-		ERROR_COMMUNICATION_QUALITY_INDICATION,		//Communication quality indication error
-		ERROR_INVALID_ADDRESS						//Invalid address
+		ERROR_FILE_OPENING,                                /* Error opening the file */
+		ERROR_READING_FILE_HEADER,                         /* Error reading the file header */
+		ERROR_INCORRECT_DATA_IN_FILE,                      /* Incorrect data in the file */
+		ERROR_COMMUNICATION_QUALITY_INDICATION,            /* Communication quality indication error */
+		ERROR_INVALID_ADDRESS                              /* Invalid address */
 	};
 
 	class Map_parser {
-		Node* head{};						//Reference to the main element, i.e. ZC
-		std::vector <Node*> nodes{};		//List of all nodes
+		Node* head{};                                      /* Reference to the main element, i.e. ZC */
+		std::vector <Node*> nodes{};                       /* List of all nodes */
 	public:
 		map_parser_error init(std::string filename);
 	};
@@ -33,4 +33,4 @@ namespace mp {
 	uint64_t convert_hex_string_to_uint(const std::string& str_hex);
 }
 
-#endif //MAP_PARSER_H
+#endif /* MAP_PARSER_H */
