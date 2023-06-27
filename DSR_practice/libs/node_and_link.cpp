2a484948b5b026c70f0e7491d9e01695243cb202
chenkun007 <makarov.nikita.o@yandex.ru>
2023-06-27 11:51:00 +0300

    Removed the tab

--- DSR_practice/libs/node_and_link.cpp
+++ DSR_practice/libs/node_and_link.cpp
@@ -8,7 +8,7 @@ using namespace nal;
 Node::Node(std::string _id) :id{ _id } {};
 
 Node::Node(std::string _id, std::uint16_t _addr, Nwk_type _device_type, std::string _label) :
-	completed{ true }, id{ _id }, addr{ _addr }, device_type{ _device_type }, label{ _label } {}
+    completed{ true }, id{ _id }, addr{ _addr }, device_type{ _device_type }, label{ _label } {}
 
 /* Link */
 
--- DSR_practice/libs/node_and_link.h
+++ DSR_practice/libs/node_and_link.h
@@ -5,39 +5,39 @@
 
 namespace nal {
 
-	enum class Nwk_type {
-		NWKMAP_DEV_COORDINATOR,
-		NWKMAP_DEV_ROUTER,
-		NWKMAP_DEV_END_DEVICE,
-		NWKMAP_DEV_UNKNOWN
-	};
+    enum class Nwk_type {
+        NWKMAP_DEV_COORDINATOR,
+        NWKMAP_DEV_ROUTER,
+        NWKMAP_DEV_END_DEVICE,
+        NWKMAP_DEV_UNKNOWN
+    };
 
-	enum class Nwk_relation {
-		NWKMAP_RELATION_PARENT,
-		NWKMAP_RELATION_CHILD,
-		NWKMAP_RELATION_SIBLING,
-		NWKMAP_RELATION_PREV_CHILD,
-		NWKMAP_RELATION_UNKNOWN
-	};
+    enum class Nwk_relation {
+        NWKMAP_RELATION_PARENT,
+        NWKMAP_RELATION_CHILD,
+        NWKMAP_RELATION_SIBLING,
+        NWKMAP_RELATION_PREV_CHILD,
+        NWKMAP_RELATION_UNKNOWN
+    };
 
-	struct Link;
+    struct Link;
 
-	struct Node {
-		bool completed{ false };                                         /* Was the node completely full? */
-		std::string id{};                                                /* Name to display */
-		std::uint16_t addr{};                                            /* Network address */
-		Nwk_type device_type{ Nwk_type::NWKMAP_DEV_UNKNOWN };
-		std::string label{};
-		std::vector<Link> links{};
-		Node(std::string _id);
-		Node(std::string _id, std::uint16_t _addr, Nwk_type _device_type, std::string _label);
-	};
+    struct Node {
+        bool completed{ false };                                         /* Was the node completely full? */
+        std::string id{};                                                /* Name to display */
+        std::uint16_t addr{};                                            /* Network address */
+        Nwk_type device_type{ Nwk_type::NWKMAP_DEV_UNKNOWN };
+        std::string label{};
+        std::vector<Link> links{};
+        Node(std::string _id);
+        Node(std::string _id, std::uint16_t _addr, Nwk_type _device_type, std::string _label);
+    };
 
-	struct Link {
-		Node* node{};
-		std::uint8_t lqi{};                                               /* Link Quality Indication */
-		Nwk_relation relation{ Nwk_relation::NWKMAP_RELATION_UNKNOWN };	  /* Type of relationship */
-	};
+    struct Link {
+        Node* node{};
+        std::uint8_t lqi{};                                               /* Link Quality Indication */
+        Nwk_relation relation{ Nwk_relation::NWKMAP_RELATION_UNKNOWN };	  /* Type of relationship */
+    };
 }
 
 #endif /* NODE_AND_LINK_H */
