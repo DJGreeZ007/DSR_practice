61c994c1a89f902d16ebfa8127d7e79b8b16c94f
chenkun007 <makarov.nikita.o@yandex.ru>
2023-06-29 13:43:02 +0300

    Beginning of network model development

---
+++ DSR_practice/network.cpp
@@ -0,0 +1,9 @@
+#include "network.h"
+
+using namespace nw;
+
+network_error nw::Network::init(std::vector<mp::Node> _nodes, std::vector<mp::Link> _links)
+{
+
+    return network_error();
+}
---
+++ DSR_practice/network.h
@@ -0,0 +1,28 @@
+﻿#ifndef NETWORK_H
+#define NETWORK_H
+
+/* Libs */
+#include "map_parser.h"
+
+/* C++ libs */
+#include <vector>
+
+namespace nw {
+
+	enum network_error {
+		NO_ERRORS
+	};
+
+	class Network {
+	public:
+		/* Сreating a graph (network model) from the values returned by map_parser */
+		network_error init(std::vector<mp::Node> _nodes, std::vector<mp::Link> _links);
+
+	private:
+		std::string label{};			/* Name ZC */
+		nal::Node* head;				/* Pointer to the coordinator */
+		std::vector <nal::Node*> nodes  /* All nodes of the network */
+	};
+}
+#endif /* NETWORK_H */
+
